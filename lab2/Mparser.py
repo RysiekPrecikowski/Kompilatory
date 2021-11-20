efff77d38169b8e0505d65e4ce3e02a542cd4084
StencelMichal <michal.stencel28@gmail.com>
2021-11-20 15:01:52 +0100

    lab2

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -1,11 +1,10 @@
-
 from lab1 import scanner
 import ply.yacc as yacc
 
 tokens = scanner.tokens
 
 precedence = (
-    ("left", "ASSIGN",  'INCREMENT', 'DECREMENT', 'MULTIPLY_ASSIGN', 'DIVIDE_ASSIGN'),
+    ("left", "ASSIGN", 'INCREMENT', 'DECREMENT', 'MULTIPLY_ASSIGN', 'DIVIDE_ASSIGN'),
     ("left", "LESSER", 'GREATER', 'LESSER_EQUAL', 'GREATER_EQUAL', 'EQUALS', 'NOT_EQUAL'),
     ("left", "PLUS", 'MINUS', 'PLUS_MATRIX', 'MINUS_MATRIX'),
     ("left", "TIMES", 'DIVIDE', "TIMES_MATRIX", 'DIVIDE_MATRIX'),
@@ -20,7 +19,7 @@ precedence = (
 
 def p_error(p):
     if p:
-        print("Syntax error at line {0}: LexToken({1}, ' {2} ')".format(p.lineno, p.type, p.value))
+        print(f"Syntax error at line {p.lineno}: LexToken({p.type}, ' {p.value} ')")
     else:
         print("Unexpected end of input")
 
@@ -30,6 +29,7 @@ def p_program(p):
     program : stmt
             | program stmt
     """
+
 
 def p_stmt(p):
     """
@@ -48,46 +48,109 @@ def p_stmt(p):
               | stmt_list stmt
     """
 
+
+def p_comparison(p):
+    """
+    comparison : expr GREATER expr
+               | expr LESSER expr
+               | expr GREATER_EQUAL expr
+               | expr LESSER_EQUAL expr
+               | expr EQUALS expr
+               | expr NOT_EQUAL expr
+    """
+
+
+def p_operator(p):
+    """
+    operator : expr PLUS expr
+             | expr MINUS expr
+             | MINUS expr %prec UMINUS
+             | expr TIMES expr
+             | expr DIVIDE expr
+    """
+
+
+def p_type_recognition(p):
+    """
+    type_recognition : ID
+                     | NUMBER
+                     | FLOATNUMBER
+                     | STRING
+    """
+
+
+def p_single_operation(p):
+    """
+    single_operation : ID ASSIGN expr
+                     | ID INCREMENT expr
+                     | ID DECREMENT expr
+                     | ID MULTIPLY_ASSIGN expr
+                     | ID DIVIDE_ASSIGN expr
+    """
+
+
+def p_expression_operation(p):
+    """
+    expression_operation : expr PLUS_MATRIX expr
+                         | expr MINUS_MATRIX expr
+                         | expr TIMES_MATRIX expr
+                         | expr DIVIDE_MATRIX expr
+    """
+
+
+def p_matrix_element_operation(p):
+    """
+    matrix_element_operation : expr idx ASSIGN expr
+                             | expr idx INCREMENT expr
+                             | expr idx DECREMENT expr
+                             | expr idx MULTIPLY_ASSIGN expr
+                             | expr idx DIVIDE_ASSIGN expr
+    """
+
+
+def p_idx(p):
+    """
+    idx : LPAREN_SQ list RPAREN_SQ
+    """
+
+
+def p_list(p):
+    """
+    list : expr
+         | list COMMA expr
+    """
+
+
+def p_empty(p):
+    """
+    empty : LPAREN_SQ RPAREN_SQ
+    """
+
+
+def p_special_matrix(p):
+    """
+    special_matrix : EYE LPAREN expr RPAREN
+                   | ONES LPAREN expr RPAREN
+                   | ZEROS LPAREN expr RPAREN
+    """
+
+
 def p_epxr(p):
     """
-    expr : ID
-         | NUMBER
-         | FLOATNUMBER
-         | STRING
-         | ID ASSIGN expr
-         | ID INCREMENT expr
-         | ID DECREMENT expr
-         | ID MULTIPLY_ASSIGN expr
-         | ID DIVIDE_ASSIGN expr
-         | expr LPAREN_SQ list RPAREN_SQ ASSIGN expr
-         | expr LPAREN_SQ list RPAREN_SQ INCREMENT expr
-         | expr LPAREN_SQ list RPAREN_SQ DECREMENT expr
-         | expr LPAREN_SQ list RPAREN_SQ MULTIPLY_ASSIGN expr
-         | expr LPAREN_SQ list RPAREN_SQ DIVIDE_ASSIGN expr
-         | expr PLUS expr
-         | expr MINUS expr
-         | MINUS expr %prec UMINUS
-         | expr TIMES expr
-         | expr DIVIDE expr
-         | expr PLUS_MATRIX expr
-         | expr MINUS_MATRIX expr
-         | expr TIMES_MATRIX expr
-         | expr DIVIDE_MATRIX expr
-         | expr GREATER expr
-         | expr LESSER expr
-         | expr GREATER_EQUAL expr
-         | expr LESSER_EQUAL expr
-         | expr EQUALS expr
-         | expr NOT_EQUAL expr
-         | EYE LPAREN expr RPAREN
-         | ONES LPAREN expr RPAREN
-         | ZEROS LPAREN expr RPAREN
-         | LPAREN_SQ RPAREN_SQ
-         | LPAREN_SQ list RPAREN_SQ
+    expr : type_recognition
+         | single_operation
+         | matrix_element_operation
+         | expression_operation
+         | comparison
+         | operator
+         | special_matrix
+         | empty
+         | idx
          | expr TRANSPOSE
     list : expr
          | list COMMA expr
     """
+
 
 def p_if_stmt(p):
     """
@@ -101,6 +164,7 @@ def p_loop_stmt(p):
     range : expr RANGE expr
     for_stmt : FOR ID ASSIGN range stmt
     """
+
 
 
 def p_print(p):
